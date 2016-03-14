a276f2e44bc999a33cca99d5cfe53bfa96de5723
michael dangleterre <michael.dangleterre@gmail.com>
2016-03-14 14:26:30 +0100

    new commit 14/03/2016 - 14:26

--- pled.ps1
+++ pled.ps1
@@ -1,4 +1,5 @@
 ############ Powershell Linux Easy Deploy ############
+$version = "1.0"
 ####### Import SSH module #######
 if (Get-Command | Where {$_.Name -notmatch "New-SShSession"}){
 iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")} 
@@ -15,13 +16,49 @@ function PacMan {
         #(Invoke-SSHCommand -SessionId $sid -Command "cat /etc/os-release").Output -match "slackware" {""}      
         else {"Version de linux inconnue"}
     }
+    elseif ($hversion -match "arch-release") {"pacman -Syu"}
 }
 #######       Code       ########
-$file = Read-Host "Fichier d'inventaire"
-foreach ($i in ((Import-Csv $file -Delimiter ";" | select -Property IP).IP)) {
-    New-SSHSession -ComputerName $i -Port ((Import-Csv $file -Delimiter ";").Port)
-    $pacman = PacMan
-    foreach ($p in ((Import-Csv .\Classeur1.csv -Delimiter ";").Packages)) {
-        (Invoke-SSHCommand -SessionId ((Get-SSHSession).SessionId) -Command "$pacman install -y $p").Output
+Write-Host "Powershell Linux Easy Deploy $version"
+do {
+    [int]$menu0 = 0
+    while ($menu0 -lt 1 -or $menu0 -gt 2) {
+        Write-Host "1. Auto deploy"
+        Write-Host "2. Manual deploy"
+        Write-Host "3. Quitter"
+        [int]$menu0 = Read-Host "Votre choix ?"
+    }
+    switch($menu0){
+        1 {
+            $file = Read-Host "Fichier d'inventaire"
+            foreach ($i in ((Import-Csv $file -Delimiter ";" | select -Property IP).IP)) {
+                New-SSHSession -ComputerName $i -Port ((Import-Csv $file -Delimiter ";").Port)
+                $pacman = PacMan
+                foreach ($p in ((Import-Csv .\Classeur1.csv -Delimiter ";").Packages)) {
+                    (Invoke-SSHCommand -SessionId ((Get-SSHSession).SessionId) -Command "$pacman install -y $p").Output
+                }
+            }
+        }
+        2{
+            $nb = Read-Host "Nombre d'hôtes"
+            do {
+                $ip = Read-Host "Adresse ip du serveur distant"
+                $port = Read-Host "port ssh"
+                New-SSHSession -ComputerName $ip -Port $port
+            }
+            while ($nb -notmatch ((Get-SSHSession).SessionId).Count)
+            Write-Host "Liste des sessions SSH ouvertes"
+            Get-SSHSession
+            $id = Read-Host "Entrez l'id de la session"
+            $pacman = PacMan
+            $package = Read-Host "Quel package à installer ?"
+            Invoke-SSHCommand -SessionId ((Get-SSHSession).SessionId) -Command "$pacman install -y $p").Output
+        }
+        3{
+            foreach ($i in (Get-SSHSession).Index) {
+                Remove-SSHSession -SessionId $i
+            }
+            exit}
     }
 }
+while ($menu -notmatch "3")
