f513b1869b7a0a35cc993fa4b61661e49d2f8631
michael dangleterre <michael.dangleterre@gmail.com>
2016-03-14 10:55:15 +0100

    New commit 14/03/2016

--- pled.ps1
+++ pled.ps1
@@ -4,20 +4,22 @@ if (Get-Command | Where {$_.Name -notmatch "New-SShSession"}){
 iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")} 
 Import-Module Posh-SSH 
 #################################
-####### Custom Functions #######
-function GetPackage {
-    ## List available packages
+####### Custom Functions ########
+function PacMan {
+    $hversion = (Invoke-SSHCommand -SessionId ((Get-SSHSession).SessionId) -Command "ls /etc").Output
+    if ($hversion -match "os-release") {
+        if (((Invoke-SSHCommand -SessionId ((Get-SSHSession).SessionId) -Command "cat /etc/os-release").Output) -match "ID=ubuntu") {"apt-get"}
+        Elseif (((Invoke-SSHCommand -SessionId ((Get-SSHSession).SessionId) -Command "cat /etc/os-release").Output) -match "centos") {"yum"}
+        Elseif (((Invoke-SSHCommand -SessionId ((Get-SSHSession).SessionId) -Command "cat /etc/os-release").Output) -match "suse") {"zipper"}
+        Elseif(((Invoke-SSHCommand -SessionId ((Get-SSHSession).SessionId) -Command "cat /etc/os-release").Output) -match "gentoo") {"emerge"}
+        #(Invoke-SSHCommand -SessionId $sid -Command "cat /etc/os-release").Output -match "slackware" {""}      
+        else {"Version de linux inconnue"}
+    }
 }
-function DeployPackage {
-    ## Install package
+#######       Code       ########
+$file = Read-Host "Fichier d'inventaire"
+foreach ($host in ((Import-Csv $file -Delimiter ";" | select -Property IP).IP)) {
+    New-SSHSession -ComputerName '"'+$host+'"'
+    $package = PacMan
+    Invoke-SSHCommand -SessionId ((Get-SSHSession).SessionId) -Command $package ((Import-Csv $file -Delimiter ";" | select -Property Packages).Packages)
 }
-function RemovePackage {
-    ## Uninstall package
-}
-####### Code #######
-$nb = Read-Host "Combien d'hôtes à déployer ?"
-for ($i = 0;$i -le $nb;$i ++){
-    $Uri = Read-Host "Adresse IP"
-    New-SShSession -ComputerName -$Uri 
-}
-Get-SSHSession
